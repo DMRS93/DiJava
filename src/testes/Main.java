6dfda27b94d0f9c9cedaba69d165821056fd088f
lcfb1 <leticia.cfb@gmail.com>
2022-01-17 16:02:38 +0000

    Funcao Soma

--- src/testes/Main.java
+++ src/testes/Main.java
@@ -1,21 +1,8 @@
 package testes;
 
-import java.util.Scanner;
-
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Escolha dois numeros");
-
-		Scanner sc = new Scanner(System.in);
-
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-
-		int c = a + b;
-
-		System.out.println(c);
-		sc.close();
+		Soma.soma();
 	}
-
 }
