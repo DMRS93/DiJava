c314d9cb0f2706ca212c7b9675cbf7e7e6df82f9
Li-hub-san <ligia.baptista.santos@gmail.com>
2022-01-17 20:09:43 +0000

    Fixed features
    Added a new brach;
    Deleted all unnecessary classes;
    Added class MathOperations;
    Refactored the functions to MathOperations.

--- src/testes/Main.java
+++ src/testes/Main.java
@@ -1,9 +1,27 @@
 package testes;
+import java.util.Scanner;
 
 public class Main {
+  static Scanner scanner = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		Soma.soma();
-		Multiplicacao.multiplicacao();
-	}
+  public static void main(String[] args) {
+    System.out.print("Input a number: ");
+    int firstNumber = scanner.nextInt();
+
+    System.out.print("Input another number: ");
+    int secondNumber = scanner.nextInt();
+
+    int multiplication = MathOperations.multiplication(firstNumber,secondNumber);
+    int sum = MathOperations.sum(firstNumber, secondNumber);
+    int subtraction = MathOperations.subtraction(firstNumber, secondNumber);
+
+    System.out.println(firstNumber + " × " + secondNumber + " = " + multiplication);
+    System.out.println(firstNumber + " + " + secondNumber + " = " + sum);
+    System.out.println(firstNumber + " - " + secondNumber + " = " + subtraction);
+
+    // 'scanner.close()' should always be the last instruction.
+    // Once closed, it cannot be called anymore.
+    scanner.close();
+  }
+
 }
