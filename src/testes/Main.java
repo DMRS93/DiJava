3d801d5d3b9fbcfbdd42f1717f29c870d2a66e7e
Li-hub-san <ligia.baptista.santos@gmail.com>
2022-01-17 20:43:27 +0000

    SystemOutput
    Refactored System.out into a function

--- src/testes/Main.java
+++ src/testes/Main.java
@@ -1,27 +1,34 @@
 package testes;
+
 import java.util.Scanner;
 
 public class Main {
   static Scanner scanner = new Scanner(System.in);
 
   public static void main(String[] args) {
-    System.out.print("Input a number: ");
-    int firstNumber = scanner.nextInt();
+    int firstNumber = requestNumberInput();
+    int secondNumber = requestNumberInput();
 
-    System.out.print("Input another number: ");
-    int secondNumber = scanner.nextInt();
-
-    int multiplication = MathOperations.multiplication(firstNumber,secondNumber);
+    int multiplication = MathOperations.multiplication(firstNumber, secondNumber);
     int sum = MathOperations.sum(firstNumber, secondNumber);
     int subtraction = MathOperations.subtraction(firstNumber, secondNumber);
 
-    System.out.println(firstNumber + " × " + secondNumber + " = " + multiplication);
-    System.out.println(firstNumber + " + " + secondNumber + " = " + sum);
-    System.out.println(firstNumber + " - " + secondNumber + " = " + subtraction);
+    printOperationResult(firstNumber, secondNumber, " × ", multiplication);
+    printOperationResult(firstNumber, secondNumber, " + ", sum);
+    printOperationResult(firstNumber, secondNumber, " - ", subtraction);
 
     // 'scanner.close()' should always be the last instruction.
     // Once closed, it cannot be called anymore.
     scanner.close();
+  }
+
+  public static int requestNumberInput() {
+    System.out.print("Input a number: ");
+    return scanner.nextInt();
+  }
+
+  public static void printOperationResult(int firstNumber, int secondNumber, String operator, int result) {
+    System.out.println(firstNumber + " " + operator + " " + secondNumber + " = " + result);
   }
 
 }
